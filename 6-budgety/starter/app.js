9c1eaa518775c3bf4bec1db3d7cee91b6f98c7d5
OldRabbit736 <36852907+OldRabbit736@users.noreply.github.com>
2019-02-13 23:57:58 +0900

    Element is not Element error

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -72,7 +72,9 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -87,12 +89,16 @@ var UIController = (function () {
         },
 
         addListItem: function (obj, type) {
-            var html, newHtml;
+            var html, newHtml, element;
 
             // Create HTML string with placeholder text
             if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
                 html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             };
 
@@ -102,6 +108,7 @@ var UIController = (function () {
             newHtml = newHtml.replace('%value%', obj.value);
 
             // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentElement('beforeend', newHtml);
         },
 
         getDOMstrings: function () {
@@ -141,6 +148,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. add the new item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. calculate the budget
 
