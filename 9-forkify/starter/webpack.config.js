8c99da1b1a92089e5833f4a6a7886611bb20f5e8
OldRabbit736 <36852907+OldRabbit736@users.noreply.github.com>
2019-02-23 21:05:02 +0900

    "134. A Modern Setup: Configuring Webpack" done!!

--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const path = require('path');   // built in node module
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: './src/js/index.js', // dot is current folder (.. is going up to the parent folder)
     output: {
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, 'dist/js'),   // __dirname is the current absolute path
         filename: 'bundle.js'
-    }
+    },    
 };
