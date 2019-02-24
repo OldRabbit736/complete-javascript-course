3b069a283b091ae35aac8489b28c45e92ec94ae4
J.S. Park <sylvan0212@gmail.com>
2019-02-24 17:54:37 +0900

    "135. the webpack dev server" done!

--- 9-forkify/starter/webpack.config.js
+++ 9-forkify/starter/webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');   // built in node module
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js', // dot is current folder (.. is going up to the parent folder)
     output: {
-        path: path.resolve(__dirname, 'dist/js'),   // __dirname is the current absolute path
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),   // __dirname is the current absolute path
+        filename: 'js/bundle.js'
     },    
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'    // starting file
+        })
+    ]
 };
