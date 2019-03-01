6a229e6dcbf9ab5bf76dafe02731800738ce03a3
J.S. Park <sylvan0212@gmail.com>
2019-03-01 15:42:03 +0900

    '144. Rendering an AJAX loading spinner' done!

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,14 +1,39 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView'
+import { elements, renderLoader, clearLoader } from './views/base';
 
-async function getResult(query) {
-    const key = '4c5e5648f7ee1bf5dd0434d9869df0d9';
-    try {
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        alert(error);
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shpping list object
+ * - Linked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput();   // TODO
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearResults();
+        searchView.clearInput();
+        renderLoader(elements.searchRes);
+
+        // 4) Search for recipes
+        await state.search.getResult();
+
+        // 5) render results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result);
     }
-}
 
-getResult('rice bean');
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -1,1 +1,18 @@
-export default 'I am an exported string!!';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResult() {
+        const key = '4c5e5648f7ee1bf5dd0434d9869df0d9';
+        try {
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
